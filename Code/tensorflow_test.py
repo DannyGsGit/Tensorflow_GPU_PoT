9d239d9a1d1f10cbd3a2f2840701cacace381ae7
Unknown <danny.godbout@gmail.com>
2016-12-09 13:24:09 -0800

    Added device querying function to test code.
    New function get_available_devices() in tensorflow_test.py to query available CPUs and GPUs. Confirms proper configuration of tensorflow.

--- Code/tensorflow_test.py
+++ Code/tensorflow_test.py
@@ -24,3 +24,23 @@ print(sess.run(hello))
 a = tf.constant(10)
 b = tf.constant(32)
 print(sess.run(a + b))
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#### Confirm available devices ####
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# The above code confirms the functionality of TensorFlow, but does not guarantee
+# that our GPU(s) are properly configured. Use the following function to query
+# available devices.
+
+# Import the device library
+from tensorflow.python.client import device_lib
+
+# Function to query tensorflow for available devices
+def get_available_devices():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos]
+
+# Get the list of GPUs
+get_available_devices()
